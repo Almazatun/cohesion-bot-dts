e5fe1f3ebefc751b4aee35150f0b6de69be6770d
Almazatun <atunstyle@gmail.com>
2021-10-29 23:58:09 +0300

    Add images-scraper

--- src/commands/gif.command.ts
+++ src/commands/gif.command.ts
@@ -4,17 +4,13 @@ const instance = axios.create({
   baseURL: `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_APIKEY}&q=`,
 });
 
-function getRandomInt(max: number): number {
-  return Math.floor(Math.random() * max);
-}
-
-export async function searchGif(title: string): Promise<string> {
+export async function gifCommand(title: string): Promise<string> {
   let result: string;
   try {
     const response = await instance.get(`${title}`);
     const gifList = response.data.data;
     if (gifList.length >= 1) {
-      const index = getRandomInt(gifList.length);
+      const index = 1;
       result = gifList[index].images.original.url;
     }
   } catch (error) {
