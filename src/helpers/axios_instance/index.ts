29b5e6b960f2a2f2bf6352c6852660cd506084f9
Almazatun <atunstyle@gmail.com>
2022-02-07 17:22:07 +0300

    Add instanceVCRU

--- src/helpers/axios_instance/index.ts
+++ src/helpers/axios_instance/index.ts
@@ -3,3 +3,13 @@ import axios from 'axios';
 export const instance = axios.create({
   baseURL: `https://g.tenor.com/v1/search`,
 });
+
+export const instanceVCRU = axios.create({
+  baseURL: `https://vc.ru/`,
+  headers: {
+    'Accept-encoding': 'gzip, deflate, br',
+    // eslint-disable-next-line max-len
+    Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-language': 'en-US,en;q=0.9',
+  },
+});
